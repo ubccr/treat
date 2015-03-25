33f265ab1463405149d5f8d6a4199309f5839569
Andrew E. Bruno <aebruno2@buffalo.edu>
2015-03-25 15:05:23 -0400

    Add grand total of mutant vs non-mutant to stats output

--- cmd/treat/stats.go
+++ cmd/treat/stats.go
@@ -28,11 +28,13 @@ func Stats(dbpath, gene string) {
         }
 
         grandTotal := 0
+        nonMutant := 0
         countMap := make(map[string]int)
         totalMap := make(map[string]int)
         err = s.Search(&SearchFields{Gene: g, All: true, EditStop: -1, JuncLen: -1, JuncEnd: -1}, func (key *treat.AlignmentKey, a *treat.Alignment) {
             if a.HasMutation == uint64(0) {
                 countMap[key.Sample]++
+                nonMutant++
             }
 
             totalMap[key.Sample]++
@@ -47,14 +49,16 @@ func Stats(dbpath, gene string) {
         fmt.Println(g)
         fmt.Println(strings.Repeat("=", 80))
         fmt.Printf("%20s%11d\n", "Total Alignments:", grandTotal)
+        fmt.Printf("%20s%11d\n", "Non-Mutant:", nonMutant)
+        fmt.Printf("%20s%11d\n", "Mutant:", grandTotal-nonMutant)
         fmt.Printf("%20s%11s\n", "Edit Base:", string(tmpl.EditBase))
         fmt.Printf("%20s%11d\n", "Alt Templates:", len(tmpl.Grna))
         fmt.Printf("%20s%11d\n", "Guide RNAs:", len(tmpl.AltRegion))
         fmt.Println(strings.Repeat("-", 80))
-        fmt.Printf("%-30s%11s%11s%8s%11s%8s\n", "Sample", "Total", "OK", "%", "Mutant", "%")
+        fmt.Printf("%-25s%11s%15s%8s%11s%8s\n", "Sample", "Total", "Non-Mutant", "%", "Mutant", "%")
         fmt.Println(strings.Repeat("-", 80))
         for sample, total := range(totalMap) {
-            fmt.Printf("%-30s%11d%11d%8.2f%11d%8.2f\n", sample, total, countMap[sample], percent(countMap[sample], total), total-countMap[sample], percent(total-countMap[sample], total))
+            fmt.Printf("%-25s%11d%15d%8.2f%11d%8.2f\n", sample, total, countMap[sample], percent(countMap[sample], total), total-countMap[sample], percent(total-countMap[sample], total))
         }
 
         fmt.Println()
