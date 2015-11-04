169da125c5385425e5d566ff2a0ef644989aa6ab
Andrew E. Bruno <aebruno2@buffalo.edu>
2015-11-04 16:25:08 -0500

    Format code

--- cmd/treat/middleware.go
+++ cmd/treat/middleware.go
@@ -1,17 +1,17 @@
 // Copyright 2015 TREAT Authors. All rights reserved.
 //
 // This file is part of TREAT.
-// 
+//
 // TREAT is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // TREAT is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with TREAT.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -41,14 +41,14 @@ func DbContext(app *Application) http.Handler {
 			}).Warn("Invalid database name. Using default")
 			db, _ = app.GetDb(app.defaultDb)
 
-            session.Values[TREAT_COOKIE_DB] = app.defaultDb
-            err = session.Save(r, w)
-            if err != nil {
-                logrus.WithFields(logrus.Fields{
-                    "defaultDb": app.defaultDb,
-                    "error": err.Error(),
-                }).Error("Failed to set cookie for default db")
-            }
+			session.Values[TREAT_COOKIE_DB] = app.defaultDb
+			err = session.Save(r, w)
+			if err != nil {
+				logrus.WithFields(logrus.Fields{
+					"defaultDb": app.defaultDb,
+					"error":     err.Error(),
+				}).Error("Failed to set cookie for default db")
+			}
 		}
 
 		context.Set(r, "db", db)
