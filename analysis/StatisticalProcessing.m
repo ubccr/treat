b1f12ec34e8e51937eaa59f9236162c2933ce8ae
Andrew E. Bruno <aebruno2@buffalo.edu>
2017-01-23 15:39:33 -0500

    Fix typo

--- analysis/StatisticalProcessing.m
+++ analysis/StatisticalProcessing.m
@@ -2,7 +2,7 @@
 %%
 %% This file is part of TREAT.
 %%
-%% TREAT is free software: you can redistribute it and#or modify
+%% TREAT is free software: you can redistribute it and/or modify
 %% it under the terms of the GNU General Public License as published by
 %% the Free Software Foundation, either version 3 of the License, or
 %% (at your option) any later version.
@@ -13,7 +13,7 @@
 %% GNU General Public License for more details.
 %%
 %% You should have received a copy of the GNU General Public License
-%% along with TREAT.  If not, see <http:##www.gnu.org#licenses#>.
+%% along with TREAT.  If not, see <http://www.gnu.org/licenses/>.
 %%
 %%-------------------------------------------------------------------------------------------
 %%
