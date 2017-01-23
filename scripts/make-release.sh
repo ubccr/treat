bb20985da8f9baddbeff5fa5b49bc002bd3d31f6
Andrew E. Bruno <aebruno2@buffalo.edu>
2017-01-23 09:40:49 -0500

    Update script for cross compiling to use GOOS/GOARCH

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-source $GOROOT/src/golang-crosscompile/crosscompile.bash
 TREAT_DIR='./.treat-release'
 VERSION=`git describe --long --tags --dirty --always | sed -e 's/^v//'`
 
-for arch in linux-amd64 darwin-amd64 windows-amd64 windows-386
+for os in linux darwin windows
 do
-    NAME=treat-${VERSION}-${arch}
-    REL_DIR=${TREAT_DIR}/${NAME}
-    GO_BIN="go-${arch}"
-    cd ./cmd/treat && ${GO_BIN} build -ldflags "-X main.TreatVersion=$VERSION" . 
-    cd ../../
-    rm -Rf ${TREAT_DIR}
-    mkdir -p ${REL_DIR}
-    cp ./cmd/treat/treat* ${REL_DIR}/ 
-    cp ./README.rst ${REL_DIR}/ 
-    cp ./AUTHORS.rst ${REL_DIR}/ 
-    cp ./ChangeLog.rst ${REL_DIR}/ 
-    cp ./LICENSE ${REL_DIR}/ 
-    cp -R ./docs ${REL_DIR}/ 
-    cp -R ./examples ${REL_DIR}/ 
-    cp -R ./cmd/treat/templates ${REL_DIR}/ 
+    for arch in amd64 386
+    do
+        NAME=treat-${VERSION}-${os}-${arch}
+        REL_DIR=${TREAT_DIR}/${NAME}
+        cd ./cmd/treat && GOOS=$os GOARCH=$arch go build -ldflags "-X main.TreatVersion=$VERSION" .
+        cd ../../
+        rm -Rf ${TREAT_DIR}
+        mkdir -p ${REL_DIR}
+        cp ./cmd/treat/treat* ${REL_DIR}/ 
+        cp ./README.rst ${REL_DIR}/ 
+        cp ./AUTHORS.rst ${REL_DIR}/ 
+        cp ./ChangeLog.rst ${REL_DIR}/ 
+        cp ./LICENSE ${REL_DIR}/ 
+        cp -R ./docs ${REL_DIR}/ 
+        cp -R ./examples ${REL_DIR}/ 
+        cp -R ./cmd/treat/templates ${REL_DIR}/ 
 
-    cd ${TREAT_DIR} && zip -r ${NAME}.zip ${NAME}
-    mv  ${NAME}.zip ../
-    cd ../
-    rm -Rf ${TREAT_DIR}
-    rm ./cmd/treat/treat*
+        cd ${TREAT_DIR} && zip -r ${NAME}.zip ${NAME}
+        mv  ${NAME}.zip ../
+        cd ../
+        rm -Rf ${TREAT_DIR}
+        rm ./cmd/treat/treat*
+    done
 done
